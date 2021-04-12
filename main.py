6dca5659c3b138d6247539d5a29808276a9cd745
Sudais <44402465+sudaisx@users.noreply.github.com>
2021-04-12 19:30:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
+#Imports
+import pygame
 from pprint import pprint
+from random import randint
 from dsa import Graph, Stack, Queue
 
+#Player class
+class Player():
+    def __init__(self):
+        pass
+
+    def draw(self):
+        pass
+
+#Time class
+class Time():
+    pass
+
+#Dice class
+class Dice():
+    def __init__(self):
+        pass
+
+    def roll(self):
+        pass
+
+#Setting the graph up
 graph = Graph()
 
 graph.addNodes([i for i in range(101)])
 graph.addEdges([(i, i+1, 1) for i in range(100)], True)
 
+#debugging
 pprint(graph.displayGraph())
