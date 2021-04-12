d5d9a0878c678c2fc329c707c9acc676ac032a59
Sudais <44402465+sudaisx@users.noreply.github.com>
2021-04-12 13:01:17 +0500

    added edges

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from pprint import pprint
 from dsa import Graph, Stack, Queue
 
 graph = Graph()
-graph.addNodes([i for i in range(101)])
 
+graph.addNodes([i for i in range(101)])
+graph.addEdges([(i, i+1, 1) for i in range(100)], True)
 
 pprint(graph.displayGraph())
