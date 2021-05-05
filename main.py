6d8d74a4593452cd8ad11a67ce35447922e2bed4
Sudais <44402465+sudaisx@users.noreply.github.com>
2021-05-05 22:34:19 +0500

    some changes

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 #imports
 import pygame
+from pygame import mixer
 from dsa import Graph, Stack, Queue
 from pprint import pprint
 from random import randint
+
+#initializes pygame
+pygame.init()
+
+#screen size and background image
+screen = pygame.display.set_mode((960, 540)) #creates the screen with the arguments passed as a tuple of (Width, Height)
+background = background = pygame.image.load('images/kabbu.jpg') #('Location of the background image')
+
+#icon and title
+icon = pygame.image.load('images/icon.png') #('icon location')
+pygame.display.set_caption('Kabib ke Habootars') #('Title of the game)
+pygame.display.set_icon(icon) #display icon
+
+# #Background music
+mixer.music.load('sounds/kabbu.mp3') #('file location')
+mixer.music.play(-1) #-1 so it plays in a loop
 
 #Player class
 class Player():
@@ -49,11 +66,21 @@ graph.addEdges(tile_edges, True)
 graph.addEdges(snakes, True)
 graph.addEdges(kabootars, True)
 
-total_players = int(input("How many players? Enter a number between 2-4\n"))
-players = Queue()
-
-
-print(players.q)
+# total_players = int(input("How many players? Enter a number between 2-4\n"))
+# players = Queue()
 
 #debugging
 pprint(graph.displayGraph())
+
+#Game loop
+running = True #Initialise with True to run the gamme
+while running: #checks if game is still running
+    screen.fill((45, 48, 51)) #draw a background of color (r, g, b)
+    screen.blit(background, (0,0)) #draw background image
+
+    #Check keystrokes
+    for event in pygame.event.get(): #iterates through all the eventss in event.get()
+        if event.type == pygame.QUIT: #Checks if the X button has been pressed on the window, if yes then
+            running = False #sets running to False so the game breaks
+
+    pygame.display.update() #updates display within the loop
