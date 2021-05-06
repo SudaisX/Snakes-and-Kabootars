ac5a2514fa2083132713e25f141010bbbc1f213a
Murtaza <itsmemurtaza@hotmail.com>
2021-05-07 00:18:39 +0500

    Update 2
    fixed the position error for the background.

--- Start Screen.py
+++ Start Screen.py
@@ -5,22 +5,22 @@ pygame.init()
 clock = pygame.time.Clock()
 
 #Screen
-WIDTH = 715
-HEIGHT = 765
+WIDTH = 800
+HEIGHT = 650
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption('HU Kabootars les gooo')
         #BAckground
-background = pygame.image.load(r"images\Board.png")
+background = pygame.image.load(r"images\Kabootars\kabbu.jpg")
 
 def start_menu():
     hellotext = pygame.font.SysFont("Arial",30).render('Hello',True, 'white')
     while True: #keeps running after called
         screen.fill("black")  #default colour
-        screen.blit(hellotext,WIDTH//2,HEIGHT//2)    #test text
-        screen.blit(background,0,0) #blit the background
+        screen.blit(hellotext,(5,5))    #test text
+        screen.blit(background,(-500,-100)) #blit the background
 
         for event in pygame.event.get():
-            if event.type == pygame.QUIT():
+            if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
         pygame.display.update()
@@ -32,7 +32,7 @@ def start_menu():
 while True:
     start_menu()
     for event in pygame.event.get():
-        if event.type == pygame.QUIT():
+        if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
     pygame.display.update()
