317de1697f86f3963f1e5f5cf0ba8762ffc4b66a
Murtaza <itsmemurtaza@hotmail.com>
2021-05-06 21:44:51 +0500

    Some trial and error changes to my own board thingie

--- Board Murts.py
+++ Board Murts.py
@@ -1,8 +1,10 @@
 import pygame
-
-WIDTH = HEIGHT = 512
+from pprint import *
+WIDTH = HEIGHT = 600
 DIMENSION = 10
 BLOCK_SIZE = WIDTH // DIMENSION
+IMAGES = {}
+FPS = 60
 
 class GameEngine():
     def __init__(self):
@@ -19,13 +21,40 @@ class GameEngine():
             ['start','--','--','--','--','--','--','--','--','--']
         ]
 
+def image_load():
+    IMAGES['--'] = pygame.image.load(r"C:\Users\Student\Documents\GitHub\DSA-Projecto\images\box_4.png")
 
+    
 def main():
     pygame.init()
     screen = pygame.display.set_mode((WIDTH,HEIGHT))
     clock = pygame.time.Clock()
-    screen.fill(pygame.Color('white'))
+    screen.fill(pygame.Color('black'))
     game = GameEngine()
-    print(game.board())
+    image_load()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        drawGame(screen, game)
+        clock.tick(FPS)
+        pygame.display.flip()
+        
+def drawGame(screen, game):
+    drawBoard(screen)
 
+
+def drawBoard(screen):
+    image = pygame.image.load(r'C:\Users\Student\Documents\GitHub\DSA-Projecto\images\box_4.png')
+    image = pygame.transform.scale(image, (WIDTH//4,HEIGHT//4))
+    image.convert()
+    rect = image.get_rect()
+    rect.center = DIMENSION//4,DIMENSION//4
+    color_list = [pygame.Color('blue'),pygame.Color('orange')]
+    for row in range(DIMENSION):
+        for column in range(DIMENSION):
+            color = color_list[(row+column) %2]
+            screen.blit(image,rect)
+            pygame.draw.rect(screen, 'white', rect,1)
 main()
