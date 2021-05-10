21e15543bd4eb7b1fedd1c33cf6c3235aeb45457
Murtaza <itsmemurtaza@hotmail.com>
2021-05-10 18:04:38 +0500

    Update 7
    Produced Second Screen. Mains done, few touchups and I'll merge with main.

--- Start Screen.py
+++ Start Screen.py
@@ -9,30 +9,30 @@ WIDTH = 800
 HEIGHT = 650
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption('HU Kabootars les gooo')
-        #BAckground
-background = pygame.image.load(r"images\Kabootars\memeKabootar.jpg")
+        #BAckground_start_menu
+background = pygame.image.load(r"images\Kabootars\kabbu.jpg")
         #font
-font = pygame.font.SysFont('Fipps',30)
+font = pygame.font.Font(r'Fonts\Fipps-Regular.otf',20)
 
 def start_menu():
     hellotext = font.render('Hello',True, 'white')
+    running = True
     while True: #keeps running after called
         pygame.event.get()
         screen.fill("black")  #default colour
-        screen.blit(hellotext,(5,5))    #test text
-        screen.blit(background,(-150,50)) #blit the background
+        # screen.blit(hellotext,(5,5))    #test text
+        screen.blit(background,(-500,-100)) #blit the background
 
-        button_text = font.render("Start!",True,'Red') # start button text
-        screen.blit(button_text,(500,10))
+        button_text = font.render("Start!",True,'White') # start button text
+        start_button = create_button(500,50,button_text.get_width(),button_text.get_height(),'Gold','Purple')
+        screen.blit(button_text,(500,50))
 
-        start_button = create_button(500,10,button_text.get_width(),button_text.get_height(),'Blue','Blue')
         if start_button:
             player_select()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.display.quit()
-                pygame.quit()
+                running = False
                 exit()
 
         pygame.display.update()
@@ -47,32 +47,36 @@ def create_button(x, y, width, height, activecolor, inactivecolor): # text = wha
         pygame.draw.rect(screen,activecolor,(x,y,width,height))         #light up the button
         if click[0] == 1:
             return True                                         #when the click is on the button move to player choosing screen
-        else:
-            pygame.draw.rect(screen, activecolor,(x,y,width,height)) #when not hovering keep it dormant
+    else:
+        pygame.draw.rect(screen, inactivecolor,(x,y,width,height)) #when not hovering keep it dormant
 
 
 def player_select(): # second screen
-    while True:
+    running = True
+    while running:
         pygame.event.get()
-        screen.fill('Black')
-        single_player_text = font.render('1 player',True,'Blue')
-        screen.blit(single_player_text,(0,0))
-        single_player = create_button(0,0,single_player_text.get_width(),single_player_text.get_height(),'Yellow','Green')
+        screen.fill('Black')  
+        screen.blit(background,(-500,-100))  
+        single_player_text = font.render('1 player',True,'White')
+        double_player_text = font.render('2 players',True,'White')
+        player_select_prompt = font.render('How many players will be playing?',True,'White')
+        screen.blit(player_select_prompt,(125,300))
+        single_player = create_button(150,500,single_player_text.get_width(),single_player_text.get_height(),'Gold','Purple')
+        screen.blit(single_player_text,(150,500))
+        double_player = create_button(450,500,double_player_text.get_width(),double_player_text.get_height(),'Gold',"Purple")
+        screen.blit(double_player_text,(450,500))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.display.quit()
-                pygame.quit()
-                exit()
+                running =False
         pygame.display.update()
         clock.tick(15)
 
 # runner
-while True:
+running = True
+while running:
     start_menu()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.display.quit()
-            pygame.quit()
-            exit()
+            running = False
     pygame.display.update()
     clock.tick(15)
